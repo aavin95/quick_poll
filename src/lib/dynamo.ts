c80dba9a3e5f5f0ae7471ad2c31186bb0d2d0c40
aavin@umich.edu <aavin@umich.edu>
2024-11-19 12:19:39 -0500

    fixed file mix up

--- src/lib/dynamo.ts
+++ src/lib/dynamo.ts
@@ -2,7 +2,14 @@
 import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
 import { DynamoDBDocumentClient } from "@aws-sdk/lib-dynamodb";
 
-const client = new DynamoDBClient({ region: "us-east-2" }); // Specify your AWS region
+const client = new DynamoDBClient({
+  region: process.env.NEXT_PUBLIC_CLOUD_REGION,
+  credentials: {
+    accessKeyId: process.env.NEXT_PUBLIC_CLOUD_KEY_ID || "",
+    secretAccessKey: process.env.NEXT_PUBLIC_CLOUD_ACCESS_KEY || "",
+  },
+});
+
 const dynamoDb = DynamoDBDocumentClient.from(client, {
   marshallOptions: {
     removeUndefinedValues: true,
